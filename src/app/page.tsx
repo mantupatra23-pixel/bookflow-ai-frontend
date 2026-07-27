9a78fea6c78be17fac215214207b978ac90d4fa6
mantupatra23-pixel <mantupatra23-pixel@github.com>
2026-07-27 22:17:47 +0530

    Use direct relative import for firebase module

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { auth, googleProvider } from "@/lib/firebase";
+import { auth, googleProvider } from "../lib/firebase";
 import { signInWithPopup, signOut, onAuthStateChanged } from "firebase/auth";
 
 export default function Home() {
